116920ab8c3d35dc9785e6b5fd8909b0949fcd34
Hiram Golze <hbgolze@gmail.com>
2016-10-28 17:14:39 -0500

    Modifications in answer structures and other improvements

--- randomtest/models.py
+++ randomtest/models.py
@@ -33,20 +33,22 @@ class Solution(models.Model):
     problem_label = models.CharField(max_length=20,blank=True)
     tags = models.ManyToManyField(Tag,blank=True)
 
-class Answer(models.Model):
-    answer = models.CharField(max_length=10,blank=True)
+class Response(models.Model):
+    response = models.CharField(max_length=10,blank=True)
     problem_label = models.CharField(max_length=20)
     def __str__(self):
-        return self.answer
+        return self.response
 
 class Problem(models.Model):
     problem_number = models.IntegerField()
     tags = models.ManyToManyField(Tag,related_name='problems')
     year = models.IntegerField()
-    types = models.ManyToManyField(Type)#type (AMC8,AMC10,AMC12,AIME,USAMO, etc... multiple types...?)
+    types = models.ManyToManyField(Type)#allows for  multiple types, though this isn't really utilized. 
     answer = models.CharField(max_length=20)
     label = models.CharField(max_length=20)
+    readable_label = models.CharField(max_length=20,blank=True)
     problem_text = models.TextField(blank=True)
+    answer_choices = models.TextField(blank=True)
     form_letter = models.CharField(max_length=2,blank=True)
     test_label = models.CharField(max_length=20,blank=True)
     question_type = models.ManyToManyField(QuestionType)
@@ -65,8 +67,6 @@ class Problem(models.Model):
 class Test(models.Model):
     name = models.CharField(max_length=50)#Perhaps use a default naming scheme
     problems = models.ManyToManyField(Problem)
-    answers = models.ManyToManyField(Answer,blank=True,related_name='parent_test')
-    num_problems_correct = models.IntegerField()
     types = models.ManyToManyField(Type)
     created_date = models.DateTimeField(default = timezone.now)
     last_attempted_date = models.DateTimeField(default = timezone.now)
@@ -80,20 +80,26 @@ class Test(models.Model):
         for i in range(1,len(L)):
             s += ', '+L[i].type
         return s
-    def delete(self, *args, **kwargs):
-        A=list(self.answers.all())
-        for i in range(0,len(A)):
-            A[i].delete()
-        super(Test, self).delete(*args, **kwargs)
+
+
+class Responses(models.Model):
+    test = models.ForeignKey(Test,on_delete=models.CASCADE)
+    responses = models.ManyToManyField(Response,blank=True)
+    num_problems_correct = models.IntegerField(blank=True,null=True)
+    def __str__(self):
+        return self.test.name
 
 class UserProfile(models.Model):
     # This line is required. Links UserProfile to a User model instance.
     user = models.OneToOneField(User)
     tests = models.ManyToManyField(Test, blank=True)
-    # The additional attributes we wish to include.
-#    website = models.URLField(blank=True)
-#    picture = models.ImageField(upload_to='profile_images', blank=True)
+    allresponses = models.ManyToManyField(Responses,blank=True,related_name='user_profile')
 
-    # Override the __unicode__() method to return out something meaningful!
     def __unicode__(self):
         return self.user.username
+
+def get_or_create_up(user):
+    userprofile,boolcreated=UserProfile.objects.get_or_create(user=user)
+    if boolcreated==False:
+        userprofile.save()
+    return userprofile
