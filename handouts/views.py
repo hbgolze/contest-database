b249655b734d2f191a6b2019b7c6272e3650dcab
Hiram Golze <hbgolze@gmail.com>
2017-09-05 00:36:38 -0600

    minor fix

--- handouts/views.py
+++ handouts/views.py
@@ -17,7 +17,7 @@ from django.views.generic import UpdateView,DetailView,ListView,CreateView
 from .forms import SectionForm,SubsectionForm,TextBlockForm,TheoremForm,ProofForm,HandoutForm,ImageForm
 from .models import Handout,Section,DocumentElement,SubSection,TextBlock,Theorem,Proof,ImageModel
 from randomtest.utils import newtexcode,compileasy
-from randomtest.models import get_or_create_up,SortableProblem,NewTest,Type,Tag
+from randomtest.models import get_or_create_up,SortableProblem,NewTest,Type,Tag,Problem
 
 # Create your views here.
 
