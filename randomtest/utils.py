e3a82ac6df29b55f8bda061f5da0f5a599de9d81
Hiram Golze <hbgolze@gmail.com>
2018-02-20 00:08:08 -0700

    script fixes; enumerate/itemize parser fixed

--- randomtest/utils.py
+++ randomtest/utils.py
@@ -38,14 +38,50 @@ def tagindexpairs(latextag,s,optional=''):
     if optional=='':
         starts=indexesof('\\begin{'+latextag+'}',s)
         ends=indexesof('\\end{'+latextag+'}',s)
-        return [(starts[i],ends[i]) for i in range(0,len(starts))]
+        return [(starts[i],ends[i]) for i in range(0,min(len(ends),len(starts)))]
     else:
         starts=indexesof('\\begin{'+latextag+'}['+optional+']',s)
         ends=[]
         for i in range(0,len(starts)):
             if '\\end{'+latextag+'}' in s[starts[i]:]:
                 ends.append(s[starts[i]:].index('\\end{'+latextag+'}')+starts[i])
-        return [(starts[i],ends[i]) for i in range(0,len(ends))]
+        return [(starts[i],ends[i]) for i in range(0,min(len(starts),len(ends)))]
+
+def firstleveltagindexpairs(latextag,s,optional=''):
+    if optional=='':
+        starts = indexesof('\\begin{'+latextag+'}',s)
+        ends = indexesof('\\end{'+latextag+'}',s)
+    else:
+        starts=indexesof('\\begin{'+latextag+'}['+optional+']',s)
+        ends=[]
+        for i in range(0,len(starts)):
+            if '\\end{'+latextag+'}' in s[starts[i]:]:
+                ends.append(s[starts[i]:].index('\\end{'+latextag+'}')+starts[i])
+    if len(starts) == 0:
+        return []
+    firstlevelstarts = [starts[0]]
+    firstlevelends = []
+    level = 1
+    startindex = 1
+    endindex = 0
+    while startindex < len(starts):
+        if starts[startindex] < ends[endindex]:
+            startindex += 1
+            level += 1
+        else:
+            level -= 1
+            if level == 0:
+                firstlevelends.append(ends[endindex])
+                firstlevelstarts.append(starts[startindex])
+                startindex += 1
+                level += 1
+            endindex += 1
+    while endindex < len(ends):
+        level -=1
+        if level == 0:
+            firstlevelends.append(ends[endindex])
+        endindex += 1
+    return [(firstlevelstarts[i],firstlevelends[i]) for i in range(0,min(len(firstlevelends),len(firstlevelstarts)))]
 
 
 def asyreplacementindexes(s):
@@ -68,22 +104,204 @@ def centerasyreplacementindexes(s):
             replacementpairs.append((startindex,endindex))
     return replacementpairs
 
+
+def itemenum_beginend_pop(s,start_index = 0):
+    D = {}
+    token = ""
+    s_mod = s[start_index:]
+    token_present = 0
+    if '\\begin{enumerate}[' in s_mod:
+        token_present = 1
+        D['begin_enum_with_token'] = s_mod.index('\\begin{enumerate}[')+start_index
+        start_here = s_mod[D['begin_enum_with_token']:]
+        if ']' in start_here:
+            token = start_here[start_here.index('[')+1:start_here.index(']')]
+    if '\\begin{enumerate}' in s_mod:
+        if token_present == 1:
+            if s_mod.index('\\begin{enumerate}')+start_index < D['begin_enum_with_token']:
+                D['begin_enum'] = s_mod.index('\\begin{enumerate}')+start_index
+        else:
+            D['begin_enum'] = s_mod.index('\\begin{enumerate}')+start_index
+    if '\\begin{itemize}' in s_mod:
+        D['begin_itemize'] = s_mod.index('\\begin{itemize}')+start_index
+    if '\\end{enumerate}' in s_mod:
+        D['end_enum'] = s_mod.index('\\end{enumerate}')+start_index
+    if '\\end{itemize}' in s_mod:
+        D['end_itemize'] = s_mod.index('\\end{itemize}')+start_index
+    if '\\item ' in s_mod:
+        D['item'] = s_mod.index('\\item ')+start_index
+    min_index = -1
+    name = ""
+    for i in D:
+        if min_index == -1 or D[i] < min_index:
+            min_index = D[i]
+            name = i
+    return (name,min_index,token)
+
+def replace_enumitem(s):
+    t = itemenum_beginend_pop(s)
+    r = ""
+    level = 0
+    enum_level = 0
+    item_level = 0
+    counter = 0
+    current_pop_start = 0
+    if t[0] != "" and t[0][0:4] == "begi":# only do stuff if the first delimiter is a begin.
+        while t[0] != "":#pop til popping yields nothing
+            t = itemenum_beginend_pop(s,current_pop_start)
+            if level == 0 and t[0][0:4] == "begi":
+                r += s[0:t[1]]#before the first environment opens, place in return string
+                level +=1
+                s = s[t[1]:]
+                print(t[0])
+                if t[0] == "begin_enum_with_token":
+                    enum_level += 1
+                    s = s[s.index(']')+1:]
+                    current_pop_start = 0
+                    print(t[2])
+                    if t[2] == '(i)':
+                        r += "<ol class=\"par-list-lower-roman\">"
+                    elif t[2] == '(a)':
+                        r += "<ol class=\"par-list-lower-alpha\">"
+                    elif t[2] == '(1)':
+                        r += "<ol class=\"par-list-decimal\">"
+                    elif t[2] == '(A)':
+                        r += "<ol class=\"par-list-upper-alpha\">"
+                    elif t[2] == '(I)':
+                        r += "<ol class=\"par-list-upper-roman\">"
+                    elif t[2] == 'i)':
+                        r += "<ol class=\"right-par-list-lower-roman\">"
+                    elif t[2] == 'a)':
+                        r += "<ol class=\"right-par-list-lower-alpha\">"
+                    elif t[2] == '1)':
+                        r += "<ol class=\"right-par-list-decimal\">"
+                    elif t[2] == 'A)':
+                        r += "<ol class=\"right-par-list-upper-alpha\">"
+                    elif t[2] == 'I)':
+                        r += "<ol class=\"right-par-list-upper-roman\">"
+                    elif t[2] == 'i.':
+                        r += "<ol class=\"dot-list-lower-roman\">"
+                    elif t[2] == 'a.':
+                        r += "<ol class=\"dot-list-lower-alpha\">"
+                    elif t[2] == '1.':
+                        r += "<ol class=\"dot-list-decimal\">"
+                    elif t[2] == 'A.':
+                        r += "<ol class=\"dot-list-upper-alpha\">"
+                    elif t[2] == 'I.':
+                        r += "<ol class=\"dot-list-upper-roman\">"
+                    else:
+                        r += "<ol>"# work to be done...
+                elif t[0] == "begin_enum":
+                    enum_level += 1
+                    s = s[s.index('}')+1:]
+                    current_pop_start = 0
+                    r += "<ol>"            
+                elif t[0] == "begin_itemize":
+                    item_level += 1
+                    s = s[s.index('}')+1:]
+                    current_pop_start = 0
+                    r += "<ul>"
+            t = itemenum_beginend_pop(s,current_pop_start)
+            while level > 0 and t[0] != "":#does stuff until the enum/itemize gets closed/popping yields nothing
+                t = itemenum_beginend_pop(s,current_pop_start)
+                if t[0][0:4] == "item":#first, if items are in the current level, fix them up.
+#                    print('item')
+                    if level == 1:
+                        if counter == 0:
+                            r += s[0:t[1]] + '<li>'
+                            s = s[t[1]+5:]# cut off \\item ; 
+                            current_pop_start = 0
+                            counter += 1
+                        else:
+                            r += s[0:t[1]] + '</li><li>'
+                            s = s[t[1]+5:]
+                            current_pop_start = 0
+                            counter += 1
+                    if level > 1:#if items are bad, move on to the next pop
+                        current_pop_start = t[1] + 1
+                if t[0][0:4] == "begi":
+                    level += 1#always increase the level if popping is a new begin
+                    if t[0] == "begin_enum_with_token":
+                        enum_level += 1
+                    elif t[0] == "begin_enum":
+                        enum_level += 1
+                    elif t[0] == "begin_itemize":
+                        item_level += 1
+                    if level == 2:#for the first nested situation.
+                        level_two_start = t[1]
+                    current_pop_start = t[1] + 1#move on to the next pop...
+                if t[0][0:4] == "end_":
+#                    print('===================',s)
+                    level -= 1# always decrease a level
+                    if t[0] == "end_enum":
+                        enum_level -= 1
+                    elif t[0] == "end_itemize":
+                        item_level -= 1
+                    if level == 1:
+                        if t[0] == 'end_enum':
+                            level_two_end = t[1] + 15
+                        else:
+                            level_two_end = t[1] + 13
+                        r += s[0:level_two_start]
+                        r += replace_enumitem(s[level_two_start:level_two_end])
+                        s = s[level_two_end:]
+#                        print('post string',s)
+                        current_pop_start = 0
+                    elif level == 0:
+                        if t[0] == 'end_enum':
+                            r += s[0:t[1]]+'</li></ol>'
+                            s = s[t[1]+15:]
+                            current_pop_start = 0
+                            counter = 0
+                        elif t[0] == 'end_itemize':
+                            r += s[0:t[1]]+'</li></ul>'
+                            s = s[t[1]+13:]
+                            current_pop_start = 0
+                            counter = 0
+                    elif level > 1:
+                        current_pop_start = t[1]+1
+        return r+s[0:]
+    else:
+        return s
+    return s
+            
+#perhaps include '\\item' in the above parser...
+        
+
+####Strategy: pop off the first begin (itemize/enumerate); keep on popping off the next begin/end.
 def replaceitemize(s):
-    itemizes=tagindexpairs('itemize',s)
+#    itemizes=tagindexpairs('itemize',s)
+#    print(itemizes)
+    itemizes = firstleveltagindexpairs('itemize',s)
+    print(itemizes)
     if len(itemizes)==0:
         return s
     r=s[0:itemizes[0][0]]
-    for i in range(0,len(itemizes)-1):
-        middle=s[itemizes[i][0]:itemizes[i][1]+13]
-        middle=middle.replace('\\begin{itemize}','<ul>').replace('\\end{itemize}','</ul>').replace('\\item','<li>')
-        middlelist=middle.split("<li")
-        mid=middlelist[0]
-        for j in range(1,len(middlelist)-1):
-            mid+="<li"+middlelist[j]+"</li>"
-        mid+="<li"+middlelist[-1]
-        mid=mid.replace('</ul>','</li></ul>')
-        end=s[itemizes[i][1]+13:itemizes[i+1][0]]
-        r+=mid+end
+    for i in range(0,len(itemizes)-1):# this code has issues with nesting...partially an issue with tag index pairs
+        middle=s[itemizes[i][0]+15:itemizes[i][1]]
+        middle_itemizes = firstleveltagindexpairs('itemize',middle)
+        if len(middle_itemizes) == 0:
+            middle="<ul>"+middle.replace('\\item','<li>')+"</ul>"
+            middlelist=middle.split("<li")
+            mid=middlelist[0]
+            for j in range(1,len(middlelist)-1):
+                mid+="<li"+middlelist[j]+"</li>"
+            mid+="<li"+middlelist[-1]
+            mid=mid.replace('</ul>','</li></ul>')
+            end=s[itemizes[i][1]+13:itemizes[i+1][0]]
+            r+=mid+end
+        else:
+            mid_start = "<ul>" + middle[0:middle_itemizes[0][0]].replace("\\item","<li>")
+            mid_start_list = mid_start.split("<li")
+            beg = mid_start_list[0]
+            for j in range(1,len(mid_start_list)-1):
+                beg += "<li"+mid_start_list[j]+"</li>"
+            beg + "<li"+mid_start_list[-1]
+            r += beg
+#            for k in range(0,len(middle_itemizes)):
+                
+
+
     middle=s[itemizes[-1][0]:itemizes[-1][1]+13]
     middle=middle.replace('\\begin{itemize}','<ul>').replace('\\end{itemize}','</ul>').replace('\\item','<li>')
     middlelist=middle.split("<li")
@@ -125,11 +343,20 @@ def replaceenumerate(s,optional=''):
         r+=mid+'\n'+end
         return r
     else:
+        if '(' in optional:
+            parentheses = True
         token = optional.replace(')','').replace('(','').replace('.','')
+        token = ""
         r=s[0:enums[0][0]]
         for i in range(0,len(enums)-1):
             middle=s[enums[i][0]:enums[i][1]+15]
-            middle=middle.replace('\\begin{enumerate}['+optional+']','<ol type=\"'+optional.replace(')','').replace('(','').replace('.','')+'\">').replace('\\end{enumerate}','</ol>').replace('\\item ','<li type=\"'+token+'\">').replace('\\item[(1)]','<li type=\"'+token+'\">').replace('\\item[(2)]','<li type=\"'+token+'\">').replace('\\item[(3)]','<li type=\"'+token+'\">').replace('\\item[(4)]','<li type=\"'+token+'\">').replace('\\item[(5)]','<li type=\"'+token+'\">').replace('\\item[(a)]','<li type=\"'+token+'\">').replace('\\item[(b)]','<li type=\"'+token+'\">').replace('\\item[(c)]','<li type=\"'+token+'\">').replace('\\item[(d)]','<li type=\"'+token+'\">').replace('\\item[(e)]','<li type=\"'+token+'\">').replace('\\item[(i)]','<li type=\"'+token+'\">').replace('\\item[(ii)]','<li type=\"'+token+'\">').replace('\\item[(iii)]','<li type=\"'+token+'\">').replace('\\item[(iv)]','<li type=\"'+token+'\">').replace('\\item[(v)]','<li type=\"'+token+'\">')
+            if parentheses == False:
+                middle=middle.replace('\\begin{enumerate}['+optional+']','<ol>')
+#                middle=middle.replace('\\begin{enumerate}['+optional+']','<ol type=\"'+optional.replace(')','').replace('(','').replace('.','')+'\">')
+            else:
+                middle=middle.replace('\\begin{enumerate}['+optional+']','<ol class=\"parentheses-list\"'+'>')
+ #               middle=middle.replace('\\begin{enumerate}['+optional+']','<ol class=\"parentheses-list\" type=\"'+optional.replace(')','').replace('(','').replace('.','')+'\">')
+            middle = middle.replace('\\end{enumerate}','</ol>').replace('\\item ','<li type=\"'+token+'\">').replace('\\item[(1)]','<li type=\"'+token+'\">').replace('\\item[(2)]','<li type=\"'+token+'\">').replace('\\item[(3)]','<li type=\"'+token+'\">').replace('\\item[(4)]','<li type=\"'+token+'\">').replace('\\item[(5)]','<li type=\"'+token+'\">').replace('\\item[(a)]','<li type=\"'+token+'\">').replace('\\item[(b)]','<li type=\"'+token+'\">').replace('\\item[(c)]','<li type=\"'+token+'\">').replace('\\item[(d)]','<li type=\"'+token+'\">').replace('\\item[(e)]','<li type=\"'+token+'\">').replace('\\item[(i)]','<li type=\"'+token+'\">').replace('\\item[(ii)]','<li type=\"'+token+'\">').replace('\\item[(iii)]','<li type=\"'+token+'\">').replace('\\item[(iv)]','<li type=\"'+token+'\">').replace('\\item[(v)]','<li type=\"'+token+'\">')
             middlelist=middle.split("<li")
             mid=middlelist[0]
             for j in range(1,len(middlelist)-1):
@@ -139,7 +366,14 @@ def replaceenumerate(s,optional=''):
             end=s[enums[i][1]+15:enums[i+1][0]]
             r+=mid+end
         middle=s[enums[-1][0]:enums[-1][1]+15]
-        middle=middle.replace('\\begin{enumerate}['+optional+']','<ol type=\"'+optional.replace(')','').replace('(','').replace('.','')+'\">').replace('\\end{enumerate}','</ol>').replace('\\item ','<li type=\"'+token+'\">').replace('\\item[(1)]','<li type=\"'+token+'\">').replace('\\item[(2)]','<li type=\"'+token+'\">').replace('\\item[(3)]','<li type=\"'+token+'\">').replace('\\item[(4)]','<li type=\"'+token+'\">').replace('\\item[(5)]','<li type=\"'+token+'\">').replace('\\item[(a)]','<li type=\"'+token+'\">').replace('\\item[(b)]','<li type=\"'+token+'\">').replace('\\item[(c)]','<li type=\"'+token+'\">').replace('\\item[(d)]','<li type=\"'+token+'\">').replace('\\item[(e)]','<li type=\"'+token+'\">').replace('\\item[(i)]','<li type=\"'+token+'\">').replace('\\item[(ii)]','<li type=\"'+token+'\">').replace('\\item[(iii)]','<li type=\"'+token+'\">').replace('\\item[(iv)]','<li type=\"'+token+'\">').replace('\\item[(v)]','<li type=\"'+token+'\">')
+        if parentheses == False:
+            middle=middle.replace('\\begin{enumerate}['+optional+']','<ol>')
+#            middle=middle.replace('\\begin{enumerate}['+optional+']','<ol type=\"'+optional.replace(')','').replace('(','').replace('.','')+'\">')
+        else:
+            middle=middle.replace('\\begin{enumerate}['+optional+']','<ol class=\"parentheses-list\" >')
+#            middle=middle.replace('\\begin{enumerate}['+optional+']','<ol class=\"parentheses-list\" type=\"'+optional.replace(')','').replace('(','').replace('.','')+'\">')
+
+        middle=middle.replace('\\end{enumerate}','</ol>').replace('\\item ','<li type=\"'+token+'\">').replace('\\item[(1)]','<li type=\"'+token+'\">').replace('\\item[(2)]','<li type=\"'+token+'\">').replace('\\item[(3)]','<li type=\"'+token+'\">').replace('\\item[(4)]','<li type=\"'+token+'\">').replace('\\item[(5)]','<li type=\"'+token+'\">').replace('\\item[(a)]','<li type=\"'+token+'\">').replace('\\item[(b)]','<li type=\"'+token+'\">').replace('\\item[(c)]','<li type=\"'+token+'\">').replace('\\item[(d)]','<li type=\"'+token+'\">').replace('\\item[(e)]','<li type=\"'+token+'\">').replace('\\item[(i)]','<li type=\"'+token+'\">').replace('\\item[(ii)]','<li type=\"'+token+'\">').replace('\\item[(iii)]','<li type=\"'+token+'\">').replace('\\item[(iv)]','<li type=\"'+token+'\">').replace('\\item[(v)]','<li type=\"'+token+'\">')
         middlelist=middle.split("<li")
         mid=middlelist[0]
         for j in range(1,len(middlelist)-1):
@@ -171,10 +405,11 @@ def newtexcode(texcode,label,answer_choices):
         newtexcode+=texcode[repl[-1][1]:]
     newtexcode+='<br><br>'+ansscrape(answer_choices)
     newtexcode=newtexcode.replace('\\ ',' ')
-    newtexcode=replaceitemize(newtexcode)
-    newtexcode=replaceenumerate(newtexcode,'(a)')
-    newtexcode=replaceenumerate(newtexcode,'(i)')
-    newtexcode=replaceenumerate(newtexcode)
+#    newtexcode=replaceitemize(newtexcode)
+#    newtexcode=replaceenumerate(newtexcode,'(a)')
+#    newtexcode=replaceenumerate(newtexcode,'(i)')
+#    newtexcode=replaceenumerate(newtexcode)
+    newtexcode = replace_enumitem(newtexcode)
     newtexcode=newtexcode.replace('\\begin{center}','')
     newtexcode=newtexcode.replace('\\end{center}','\n')
     return newtexcode
@@ -198,10 +433,11 @@ def newsoltexcode(texcode,label):
             three='+0_0'
         newtexcode+='<img class=\"displayed\" src=\"/media/'+label+'-'+str(len(repl))+'.png\"/>'
         newtexcode+=texcode[repl[-1][1]:]
-    newtexcode=replaceitemize(newtexcode)
-    newtexcode=replaceenumerate(newtexcode,'(a)')
-    newtexcode=replaceenumerate(newtexcode,'(i)')
-    newtexcode=replaceenumerate(newtexcode)
+#    newtexcode=replaceitemize(newtexcode)
+#    newtexcode=replaceenumerate(newtexcode,'(a)')
+#    newtexcode=replaceenumerate(newtexcode,'(i)')
+#    newtexcode=replaceenumerate(newtexcode)
+    newtexcode = replace_enumitem(newtexcode)
     newtexcode=newtexcode.replace('\\begin{center}','')
     newtexcode=newtexcode.replace('\\end{center}','\n')
     return newtexcode
