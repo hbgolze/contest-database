0c139f3fdf3bab15f481282489cc682c1b5522d3
Hiram Golze <hbgolze@gmail.com>
2018-12-14 22:26:46 -0700

    Advanced search (unfinished)

--- search/urls.py
+++ search/urls.py
@@ -6,5 +6,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.searchform, name='searchform'),
     url(r'^results/$', views.searchresults, name='searchresults'),
+    url(r'^advanced_results/$', views.advanced_searchresults, name='advancedsearchresults'),
     url(r'^results/add_to_group/$', views.add_to_group, name='add_to_group'),
+    url(r'^advanced_results/add_to_group/$', views.add_to_group, name='add_to_group'),
 ]
--- search/views.py
+++ search/views.py
@@ -26,11 +26,15 @@ from itertools import chain
 
 @login_required
 def searchform(request):
+    form = request.GET
+    advanced = False
+    if 'advanced' in form:
+        advanced = True
     userprofile = get_or_create_up(request.user)
     types = userprofile.user_type_new.allowed_types.all()
     tags = sorted(list(NewTag.objects.exclude(label='root')),key=lambda x:x.tag)
     template = loader.get_template('search/searchform.html')
-    context = {'nbar' : 'search', 'types' : types,'tags' : tags}
+    context = {'nbar' : 'search', 'types' : types,'tags' : tags, 'advanced': advanced,'userprofile':userprofile,}
     return HttpResponse(template.render(context,request))
 
 @login_required
@@ -57,7 +61,7 @@ def searchresults(request):
         current_url = z.urlencode()
         form = request.GET
         if form.get('searchform','') == "start":
-            testtype = form.get('testtype','')
+            testtype = form.get('tp','')
             type_args = testtype.split('_')
             round_or_type = type_args[0]
             rt_pk = type_args[1]
@@ -136,6 +140,113 @@ def searchresults(request):
             else:
                 for i in keywords:
                     P = P.filter(Q(problem_text__contains = i)|Q(mc_problem_text__contains = i)|Q(label = i)|Q(test_label = i))
+            P = list(P)
+            P = sorted(P,key = lambda x:(x.problem_number,x.year))
+            owned_groups = userprofile.problem_groups.all()
+            editable_groups = userprofile.editable_problem_groups.all()
+            probgroups = list(chain(owned_groups,editable_groups))
+            paginator = Paginator(P,25)
+            page = request.GET.get('page')
+            try:
+                prows = paginator.page(page)
+            except PageNotAnInteger:
+                # If page is not an integer, deliver first page.
+                prows = paginator.page(1)
+            except EmptyPage:
+                # If page is out of range (e.g. 9999), deliver last page of results.
+                prows = paginator.page(paginator.num_pages)
+            template = loader.get_template('search/searchresults.html')
+            context={'nbar' : 'search', 'rows' : prows, 'searchterm': searchterm, 'current_url' : current_url,'matchnums':len(P), 'probgroups' : probgroups,'request' : request, 'tags':NewTag.objects.exclude(tag='root')}
+            return HttpResponse(template.render(context,request))
+
+@login_required
+def advanced_searchresults(request):
+    userprofile = get_or_create_up(request.user)
+    if request.method=='GET':
+        page = request.GET.get('page')
+        z = request.GET.copy()
+        if 'page' in z:
+            del(z['page'])
+        current_url = z.urlencode()
+        form = request.GET
+        if form.get('searchform','') == "start":
+            testtypes = form.getlist('tp')
+            type_pks = []
+            round_pks = []
+            for i in testtypes:
+                type_args = i.split('_')
+                if type_args[0] == 'T':
+                    type_pks.append(type_args[1])
+                else:
+                    round_pks.append(type_args[1])
+
+            searchterm = form.get('keywords','')
+            if searchterm is None or searchterm == u'':
+                keywords = []
+            else:
+                keywords = searchterm.split(' ')
+#########TAGS
+            tag_list = form.getlist('tag')
+
+            probbegin=form.get('probbegin','')
+            if probbegin is None or probbegin==u'':
+                probbegin=0
+            else:
+                probbegin=int(probbegin)
+
+            probend=form.get('probend','')
+            if probend is None or probend==u'':
+                probend=10000
+            else:
+                probend=int(probend)
+
+            yearbegin=form.get('yearbegin','')
+            if yearbegin is None or yearbegin==u'':
+                yearbegin=0
+            else:
+                yearbegin=int(yearbegin)
+
+            yearend=form.get('yearend','')
+            if yearend is None or yearend==u'':
+                yearend=10000
+            else:
+                yearend=int(yearend)
+
+            sol_opts = form.get('sol_opts','')
+            if sol_opts == "sols":
+                P = Problem.objects.exclude(solutions = None)
+            elif sol_opts == "nosols":
+                P = Problem.objects.filter(solutions = None)
+            else:
+                P = Problem.objects.all()
+            P = P.filter(problem_number__gte = probbegin,problem_number__lte = probend).filter(year__gte = yearbegin,year__lte = yearend)
+            if len(type_pks) + len(round_pks) > 0:
+                P = P.filter(Q(type_new__pk__in = type_pks)|Q(round__pk__in=round_pks))
+            if len(tag_list) > 0:
+                every_tag = []
+                for t in tag_list:
+                    every_tag += list(NewTag.objects.filter(tag__startswith = t))
+                tag_pks = [t.pk for t in every_tag]
+                P = P.filter(newtags__in = NewTag.objects.filter(pk__in = tag_pks)).distinct()
+
+
+
+#            if 'solutionsearch' in form:
+#                if round_or_type == "T":
+#                    S = Solution.objects.filter(parent_problem__problem_number__gte = probbegin,parent_problem__problem_number__lte = probend).filter(parent_problem__year__gte = yearbegin,parent_problem__year__lte = yearend).filter(parent_problem__type_new__pk = rt_pk).distinct()
+#                else:
+#                    S = Solution.objects.filter(parent_problem__problem_number__gte = probbegin,parent_problem__problem_number__lte = probend).filter(parent_problem__year__gte = yearbegin,parent_problem__year__lte = yearend).filter(parent_problem__round__pk = rt_pk).distinct()
+#                for i in keywords:
+#                    S = S.filter(solution_text__contains = i)
+#                for i in keywords:
+#                    P = P.filter(Q(problem_text__contains = i)|Q(mc_problem_text__contains = i)|Q(label = i)|Q(test_label = i))
+#                P = Problem.objects.filter(Q(id__in = S.values('parent_problem_id'))|Q(id__in=P))
+#            else:
+#                for i in keywords:
+#                    P = P.filter(Q(problem_text__contains = i)|Q(mc_problem_text__contains = i)|Q(label = i)|Q(test_label = i))
+
+            for i in keywords:
+                P = P.filter(Q(problem_text__contains = i)|Q(mc_problem_text__contains = i)|Q(label = i)|Q(test_label = i))
             P = list(P)
             P = sorted(P,key = lambda x:(x.problem_number,x.year))
             owned_groups = userprofile.problem_groups.all()
