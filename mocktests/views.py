7ab1d2d3008bdf9f93c77e745f3f77c8137bdf7f
Hiram Golze <hbgolze@gmail.com>
2021-03-09 11:22:08 -0700

    login for mocktest

--- mocktests/views.py
+++ mocktests/views.py
@@ -3,23 +3,25 @@ from django.http import JsonResponse,HttpResponse
 from django.template.loader import render_to_string
 from django.utils import timezone
 from django.views.generic import DetailView
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 from randomtest.models import UserProfile,Problem
 from .models import MockTestFolder,MockTest,UserMockTest,UserMockTestSegment,UserMockProblem
 
+@login_required
 def index(request):
     userprofile,boolcreated = UserProfile.objects.get_or_create(user=request.user)
     folders = MockTestFolder.objects.all()
     return render(request,'mocktests/indexview.html',{'folders': folders,'userprofile': userprofile, 'nbar': 'mocktests'})
-
+@login_required
 def add_mocktest(request):
     pk = request.POST.get('pk','')
     mocktest = get_object_or_404(MockTest,pk=pk)
     userprofile = UserProfile.objects.get(user = request.user)
     umt = mocktest.add_to_user(userprofile)
     return JsonResponse({'table-row-html': render_to_string('mocktests/usermocktestrow.html',{'umt': umt, 'request': request})})
-
+@login_required
 def mocktestview(request):
     pk = request.GET.get('id','')
     usermocktest = get_object_or_404(UserMockTest,pk=pk)
@@ -29,7 +31,7 @@ def mocktestview(request):
     if usermocktest.status > 0:
         return HttpResponse("Test already started")
     return render(request,'mocktests/mocktest_view.html',{'umt': usermocktest})
-
+@login_required
 def start_mocktest(request):
     pk = request.POST.get('pk','')
     t = timezone.now()
@@ -43,7 +45,7 @@ def start_mocktest(request):
     user_segment.start_time = t
     user_segment.save()
     return JsonResponse({'problems-html':render_to_string('mocktests/mocktest_problems.html',{'user_segment':user_segment,'request':request}),'time_limit': user_segment.mock_test_segment.time_limit.seconds*1000})
-
+@login_required
 def submit_mocktest_segment(request):
     pk = request.POST.get('seg-pk','')
     t = timezone.now()
@@ -80,7 +82,7 @@ def submit_mocktest_segment(request):
         return JsonResponse({'problems-html':render_to_string('mocktests/mocktest_problems.html',{'user_segment':next_segment,'request':request}),'time_limit': next_segment.mock_test_segment.time_limit.seconds*1000,'end':0})
     else:
         return JsonResponse({'problems-html':render_to_string('mocktests/mocktest_break.html',{'user_segment':next_segment,'request':request}),'time_limit': next_segment.mock_test_segment.time_limit.seconds*1000,'end':0})
-
+@login_required
 def review_mocktest(request):
     pk = request.GET.get('id','')
     usermocktest = get_object_or_404(UserMockTest,pk=pk)
